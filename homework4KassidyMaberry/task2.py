1213828910f3b3f972be5bda5998554ff1f4cf36
MoonBasedKas <kassidy.maberry@gmail.com>
2025-04-10 22:30:38 -0600

    Done, Finish, fin, something

--- homework4KassidyMaberry/task2.py
+++ homework4KassidyMaberry/task2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from scapy.all import *
-from scapy.all import IP, TCP
 
 ip = IP(src="10.9.0.6", dst="10.9.0.5")
 tcp = TCP(sport=48336, dport=23, flags="R", seq=2560596923)
